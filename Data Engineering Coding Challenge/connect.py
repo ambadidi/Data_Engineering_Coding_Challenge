104af0119fb184e69b13c627982a572288059cb0
khmira <amine.badidi@hotmail.com>
2020-04-08 23:42:05 +0100

    c u 2morrow

--- Data Engineering Coding Challenge/connect.py
+++ Data Engineering Coding Challenge/connect.py
@@ -1,27 +1,27 @@
-from flask import Flask
-# from flask_*
-from flask_mongoalchemy import MongoAlchemy
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
-
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/theguardianDB"
-app.config['MONGO_DBNAME'] = 'SomeCollection'
-app.config['SECRET_KEY'] = 'secret_key'
-
-# db = MongoAlchemy(app)
-# if __name__ = "__main__":
-#     app.run(debug=True)
-# class Example(db.Document):
-#     name = db.StringField()
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
+# from flask import Flask
+# # from flask_*
+# from flask_mongoalchemy import MongoAlchemy
+# from flask_pymongo import PyMongo
+# from pymongo import MongoClient
 #
+# app = Flask(__name__)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/theguardianDB"
+# app.config['MONGO_DBNAME'] = 'SomeCollection'
+# app.config['SECRET_KEY'] = 'secret_key'
 #
-# if __name__ == '__main__':
-#     app.run()
-# def init_app():
-#     app = Flask(__name__)
-#     db.init_app(app)
-#     return app
+# # db = MongoAlchemy(app)
+# # if __name__ = "__main__":
+# #     app.run(debug=True)
+# # class Example(db.Document):
+# #     name = db.StringField()
+# # @app.route('/')
+# # def hello_world():
+# #     return 'Hello World!'
+# #
+# #
+# # if __name__ == '__main__':
+# #     app.run()
+# # def init_app():
+# #     app = Flask(__name__)
+# #     db.init_app(app)
+# #     return app
